727404b6cc14c88f84d9a7c16a948daa7eb4acf9
Oleksandr Turok <oleksandr_turok@epam.com>
2022-02-20 01:08:47 +0300

    Some changes

--- topic2/fill_vector/fill_vector.cpp
+++ topic2/fill_vector/fill_vector.cpp
@@ -16,9 +16,10 @@ int main()
     // Fill a vector
     std::vector<int>  vec1{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     show_vector(vec1);
-    // Create vector vec2 and fill with second part of vec1
     auto half_vec = vec1.begin() + vec1.size()/2;
-    std::vector<int> vec2(half_vec, vec1.end());
+    std::vector<int> vec2(vec1.size()/2);
+    //Fill vec2 with second part of vec1
+    std::copy(half_vec, vec1.end(), vec2.begin());
     show_vector(vec2);
     return 0;
 }
