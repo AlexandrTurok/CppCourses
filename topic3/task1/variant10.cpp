c1f721d6675163044d33a4599498b8bc0ba50b01
Oleksandr Turok <oleksandr_turok@epam.com>
2022-03-18 13:59:57 +0300

    Topic 3, task 1, variant 10

---
+++ topic3/task1/variant10.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdexcept>
+
+
+double div_d(double a, double b)
+{
+    if (b == 0)
+        throw std::overflow_error("Divide by zero exception");
+    return a/b;
+}
+
+int main()
+{
+    double a, b;
+    std::cout << "Input a and b: ";
+    std::cin >> a >> b;
+
+    if(std::cin.fail())
+    {
+        std::cerr << "Error input." << std::endl;
+        std::exit(-1);
+    }
+    try
+    {
+        double c = div_d(a, b);
+        std::cout << c << std::endl;
+    }
+    catch(std::overflow_error const& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+
+
+    return 0;
+}
